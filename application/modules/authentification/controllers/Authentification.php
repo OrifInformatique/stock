6d3db77f9777831936e60ee41c7a362747d369a1
jeffrey Mostroso <n3tsky@me.com>
2016-01-15 15:12:47 +0100

    Update of the Auth Controller

--- application/modules/authentification/controllers/Authentification.php
+++ application/modules/authentification/controllers/Authentification.php
@@ -1,45 +1,74 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Authentification extends CI_Controller
+class Authentification extends MY_Controller
 {
+
     public function __construct()
     {
         parent::__construct();
-
+        $this->load->model('authentification_model', 'authentification');
     }
 
     public function index()
     {
-        $this->login();
+        redirect('authentification/login');
     }
 
     public function login()
     {
-        if ($this->form_validation->run() == false) {
-            $this->template->build('authentification/login');
-        } else {
-            // else redirect the user to the admin panel
-            $this->session->set_flashdata('message', 'Bienvenu !');
-            redirect('dashboard', 'refresh');
+        $this->logged_in_check();
+        if ($this->form_validation->run() == true) {
+            $status = $this->authentification->validate();
+            if ($status == ERR_INVALID_USERNAME) {
+                //$this->session->set_flashdata('error', 'Username is invalid');
+                $this->session->set_flashdata('error', ['message' => 'Username is invalid']);
+            } elseif ($status == ERR_INVALID_PASSWORD) {
+                //$this->session->set_flashdata('error', 'Password is invalid');
+                $this->session->set_flashdata('error', ['message' => 'Password is invalid']);
+            } else {
+                $this->session->set_userdata($this->authentification->get_data());
+                $this->session->set_userdata("logged_in", true);
+                redirect("dashboard");
+            }
+        }
+
+        $this->load->view('login');
+    }
+
+    public function logged_in_check()
+    {
+        if ($this->session->userdata("logged_in")) {
+            redirect("dashboard");
         }
     }
 
-    public function registration()
+    // test de hash
+
+    public function pwd($password)
     {
-        if ($this->form_validation->run() == false) {
-            $this->template->build('authentification/registration');
-        } else {
-            // else redirect the user to the admin panel
-            $this->session->set_flashdata('message', 'Bienvenu !');
-            redirect('dashboard', 'refresh');
-        }
+        $password = hash("sha256", $password);
+        var_dump($password);
     }
 
-    function logout()
+    /**
+     * logout function.
+     *
+     * @access public
+     * @return void
+     */
+    public function logout()
     {
-        $this->session->unset_userdata('logged_in');
-        session_destroy();
-        redirect('home', 'refresh');
+        $data = new stdClass();
+        if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+            foreach ($_SESSION as $key => $value) {
+                unset($_SESSION[$key]);
+            }
+            $this->session->set_flashdata('error', 'Vous êtes maintenant déconnecté.');
+            redirect('authentification/login', 'refresh');
+        } else {
+            redirect('/');
+        }
+
     }
 
 }
