a9719ba6982c9e1fcb20ff349dc920829a3f5671
Didier Viret <didier.viret@orif.ch>
2018-07-11 15:46:44 +0200

    Suppress default date value in planned_return_date
    If no value is set, don't use the current date in planned_return_date field

--- application/views/loan/form.php
+++ application/views/loan/form.php
@@ -3,7 +3,7 @@
 	<input class="form-control" name="date" type="date" value="<?php if(isset($date)) {echo $date;} else {echo set_value('date', date('Y-m-d'));} ?>" /><br />
 
 	<label for="planned_return_date"><?php echo $this->lang->line('header_loan_planned_return'); ?> :&nbsp;</label>
-	<input class="form-control" name="planned_return_date" type="date" value="<?php if(isset($planned_return_date)) {echo $planned_return_date;} else {echo set_value('date');} ?>" /><br />
+	<input class="form-control" name="planned_return_date" type="date" value="<?php if(isset($planned_return_date)) {echo $planned_return_date;} ?>" /><br />
 
 	<label for="real_return_date"><?php echo $this->lang->line('header_loan_real_return'); ?> :&nbsp;</label>
 	<input class="form-control" name="real_return_date" type="date" value="<?php if(isset($real_return_date)) {echo $real_return_date;} else {echo set_value('real_return_date');} ?>" /><br />
