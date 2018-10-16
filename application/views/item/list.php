bdc08f40941ff3024cbece923f9388f75cd76e5f
MestourBS <42637780+MestourBS@users.noreply.github.com>
2018-10-16 10:45:34 +0200

    Updated to patch 1.5

--- application/views/item/list.php
+++ application/views/item/list.php
@@ -24,7 +24,7 @@
           echo form_label($this->lang->line('field_text_search'), 'text_search');
           echo form_input('ts', $ts, 
                           'id="text_search" class="form-control"
-                           placeholder="'.$this->lang->line('field_no_filter').'"');
+                           placeholder="'.$this->lang->line('field_no_filter').'" onfocusin="changeTextFocus(true)" onfocusout="changeTextFocus(false);submitOnClose()"');
         ?>
         </div>
       </div>
@@ -36,7 +36,7 @@
           echo form_label($this->lang->line('field_tags'),
                           'item_conditions-multiselect');
           echo form_dropdown('t[]', $item_tags, $t,
-                             'id="item_tags-multiselect" multiple="multiple"');
+                             'id="item_tags-multiselect" multiple="multiple" onchange="submitOnClose()"');
         ?>
         </div>
 
@@ -46,7 +46,7 @@
           echo form_label($this->lang->line('field_group'),
                           'item_groups-multiselect');
           echo form_dropdown('g[]', $item_groups, $g,
-                             'id="item_groups-multiselect" multiple="multiple"');
+                             'id="item_groups-multiselect" multiple="multiple" onchange="submitOnClose()"');
         ?>
         </div>
       </div>
@@ -58,7 +58,7 @@
             echo form_label($this->lang->line('field_stocking_place'),
                             'stocking_places-multiselect');
             echo form_dropdown('s[]', $stocking_places, $s,
-                               'id="stocking_places-multiselect" multiple="multiple"');
+                               'id="stocking_places-multiselect" multiple="multiple" onchange="submitOnClose()"');
           ?>
         </div>
 
@@ -68,7 +68,7 @@
           echo form_label($this->lang->line('field_item_condition'),
                           'item_conditions-multiselect');
           echo form_dropdown('c[]', $item_conditions, $c,
-                             'id="item_conditions-multiselect" multiple="multiple"');
+                             'id="item_conditions-multiselect" multiple="multiple" onchange="submitOnClose()"');
         ?>
         </div>
       </div>
@@ -86,7 +86,7 @@
           echo form_label($this->lang->line('field_sort_order'),
                             'sort_order');
           echo form_dropdown('o', $sort_order, $o,
-                               'id="sort_order"');
+                               'id="sort_order" onchange="submitOnClose()"');
         ?>
         </div>
       </div>
@@ -97,7 +97,7 @@
           echo form_label($this->lang->line('field_sort_asc_desc'),
                             'sort_asc_desc');
           echo form_dropdown('ad', $sort_asc_desc, $ad,
-                               'id="sort_asc_desc"');
+                               'id="sort_asc_desc" onchange="submitOnClose()"');
         ?>
         </div>
       </div>
@@ -107,8 +107,8 @@
   <!-- BUTTONS -->
   <div class="row">
     <div class="col-sm-6 col-xs-12 top-margin xs-center">
-      <button type="submit" class="btn btn-primary top-margin"><?php echo html_escape($this->lang->line('btn_submit_filters')); ?></button>
-      <a href="<?php echo base_url(); ?>item" class="btn btn-default top-margin"><?php echo html_escape($this->lang->line('btn_remove_filters')); ?></a>
+        <button type="button" onclick="submitNow()" class="btn btn-primary top-margin"><?php echo html_escape($this->lang->line('btn_submit_filters')); ?></button>
+        <button type="button" onclick="voidFilter()" class="btn btn-default top-margin"><?php echo html_escape($this->lang->line('btn_remove_filters')); ?></button>
     </div>
   </div>
 
@@ -116,9 +116,9 @@
 </form>
 
 <!-- PAGINATION -->
-<div id="pagination_top"><?=$pagination?></div>
 
 <div class="top-margin table-responsive">
+  <div id="pagination_top"><?=$pagination?></div>
   
   <!-- LIST OF ITEMS -->
   <?php if(empty($items)) { ?>
@@ -179,8 +179,8 @@
     </tbody>
   </table>
   <?php } ?>
+  <div id="pagination_bottom"><?=$pagination?></div>
 </div>
-<div id="pagination_bottom"><?=$pagination?></div>
 </div>
 
 <!-- Initialize the Bootstrap Multiselect plugin: -->
@@ -220,5 +220,89 @@
         buttonWidth: '100%',
         numberDisplayed: 5
       });
+      loadPage();
     });
+    var counter = 0,
+        textFocused = false;
+
+    function loadPage() {
+      var linksToChange = $('.pagination').children();
+      for(var i = 0; i < linksToChange.length; i++) {
+        var linkToChange = linksToChange[i].children[0];
+        if(linkToChange.attributes['href'] != null) {
+          var linkChanged = "setPage(\'" + linkToChange.attributes['href'].value + "\')";
+          linkToChange.removeAttribute('href');
+          linkToChange.setAttribute('onclick', linkChanged);
+        }
+      }
+    }
+    function setPage(newPage) {
+      $('#whatToShow').load(newPage.toString() + ' #whatToShow');
+      setTimeout(function() {loadPage();}, 2000);
+      //Delay needed to allow loadPage() to do its thing on the page
+    }
+    function voidFilter() {
+        //First, get the url
+        var targetForm = $('#filters');
+        var urlWithParams = targetForm.attr('action');
+        $('#whatToShow').load(urlWithParams +  ' #whatToShow');
+
+        //This part resets the filters
+        $('#item_conditions-multiselect option:selected').each(function() {
+          $(this).prop('selected', false);
+        });
+        $('#item_conditions-multiselect').val(10);
+        $('#item_conditions-multiselect').multiselect('refresh');
+        $('#item_tags-multiselect option:selected').each(function() {
+          $(this).prop('selected', false);
+        });
+        $('#item_tags-multiselect').multiselect('refresh');
+        $('#item_groups-multiselect option:selected').each(function() {
+          $(this).prop('selected', false);
+        });
+        $('#item_groups-multiselect').multiselect('refresh');
+        $('#stocking_places-multiselect option:selected').each(function() {
+          $(this).prop('selected', false);
+        });
+        $('#stocking_places-multiselect').multiselect('refresh');
+        $('#text_search').val("");
+
+    }
+    function loadPart() {
+        //First, get the url
+        var targetForm = $('#filters');
+        var urlWithParams = targetForm.attr('action');
+        urlWithParams += "?" + targetForm.serialize();
+        //console.log(urlWithParams);
+
+        //Then, load the new url into the whatToShow part
+        $('#whatToShow').load(urlWithParams +  ' #whatToShow');
+    }
+
+    function changeTextFocus(newFocus) {
+        textFocused = newFocus;
+    }
+
+    function submitNow() {
+      envoi(5000);
+    }
+    function submitOnClose() {
+        if(counter == 0){
+            counter++;
+        }else{
+            counter = 0;
+            //Does not prevent page usage while waiting for the user to close the selectors
+            envoi(5000); //5 seconds delay by default
+        }
+    }
+
+    function envoi(delay) {
+        if(document.getElementsByClassName('btn-group open').length == 0 && !textFocused) {
+            //Submits the form
+            loadPart();
+        }else{
+            //Function failed, launch another attempt after delay milliseconds
+            setTimeout(function() {envoi(delay)}, delay);
+        }
+    }
 </script>
