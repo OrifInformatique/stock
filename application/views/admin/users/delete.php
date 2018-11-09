2842b59302bd47ba0a3ca139eb8441143e5bb6fd
MestourBS <42637780+MestourBS@users.noreply.github.com>
2018-11-09 10:29:09 +0100

    Cannot desactive an inactive user anymore

--- application/views/admin/users/delete.php
+++ application/views/admin/users/delete.php
@@ -8,14 +8,16 @@
       <a href="<?php echo base_url(); ?>admin/view_item_groups" class="tab_unselected"><?php echo $this->lang->line('admin_tab_item_groups'); ?></a>
     </div>
   </h3>
-  <?php  if(isset($username)) { ?>
+  <?php if(isset($username)) { ?>
   <div class="row" style="margin-top: 20px;">
     <em><?php echo $this->lang->line('admin_delete_user_verify').'"'.$username.'" ?'; ?></em>
   </div>
   <div class="btn-group row" style="margin-top: 10px;">
     <a href="<?php echo base_url().uri_string()."/confirmed";?>" class="btn btn-danger btn-lg"><?php echo $this->lang->line('text_yes'); ?></a>
     <a href="<?php echo base_url()."admin/view_users/";?>" class="btn btn-lg"><?php echo $this->lang->line('text_no'); ?></a>
+    <?php if($is_active) { ?>
     <a href="<?php echo base_url().uri_string()."/d";?>" class="btn btn-warning btn-lg"><?php echo $this->lang->line('admin_desactivate_user'); ?></a>
+    <?php } ?>
   </div>
     <?php }else{ ?>
     <div class="row">
