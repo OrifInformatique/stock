58aea2e63d9491ba54ce29dd2fa8246ada6ce2a7
RemyTombez <Remy.Tombez@formation.orif.ch>
2019-11-07 16:15:47 +0100

    Added linking to camera app

--- application/views/item/photo_test.php
+++ application/views/item/photo_test.php
@@ -5,11 +5,15 @@
             <button id="snap" onclick="return false">Photo</button>
             <canvas id="canvas" width="360" height="360"></canvas>
             <input id="camera" type="file" accept="image/*" capture="camera" onchange="showPhoto()" />
-            <img id="image" />
+            <img id="image" width="360" height="360" />
+            <div id="selection" hidden></div>
         </form>
     </div>
 </div>
 <script src="<?=base_url("assets/js/photo.js");?>" type="text/javascript"></script>
-<script>
-
-</script>
+<style>
+    #selection {
+        border: 1px dotted #000;
+        position: absolute;
+    }
+</style>
--- assets/js/photo.js
+++ assets/js/photo.js
@@ -1,7 +1,10 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var video = document.getElementById('video');
+var selection = document.getElementById("selection");
+var x1 = 0, y1 = 0, x2 = 0, y2 = 0;
 
+// Record the camera's input and show it as a video
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
     navigator.mediaDevices.getUserMedia({video: true}).then(function(stream){
         video.srcObject = stream;
@@ -9,16 +12,59 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
     });
 }
 
+// Create a screenshot (360x360) of the camera when the snap button is pressed
 document.getElementById("snap").addEventListener("click", function(){
    context.drawImage(video, 0,0, 360, 360); 
 });
 
+// Setup mouse events
+onmousedown = beginSelect;
+onmousemove = changeSelect;
+onmouseup = endSelect;
+
+// Show the selection div and setup it's first point
+function beginSelect(e){
+    selection.hidden = 0;
+    x1 = e.clientX;
+    y1 = e.clientY;
+    calculateSelection();
+}
+
+// Update in real time the selection div's last point
+function changeSelect(e){
+    x2 = e.clientX;
+    y2 = e.clientY;
+    calculateSelection();
+}
+
+// Hide the selection
+function endSelect(e){
+    selection.hidden = 1;
+}
+
+// Draw the selection div
+function calculateSelection(){
+    var x3 = Math.min(x1, x2); // The first point's X position
+    var x4 = Math.max(x1, x2); // The last point's X position
+    var y3 = Math.min(y1, y2); // The first point's Y position
+    var y4 = Math.max(y1, y2); // The last point's Y position
+    
+    selection.style.left = x3 + 'px';
+    selection.style.top = y3 + 'px';
+    selection.style.width = x4 - x3 + 'px';
+    selection.style.height = y4 - y3 + 'px';
+}
+
+// Show the photo taken with the user's camera
 function showPhoto(){
     var reader = new FileReader();
     
-    reader.onload = function (e) {
-        document.getElementById("image").src = e.target.result;
-    };
+    reader.onload = setPhoto;
     
-    reader.readAsDataURL(this.result[0]);
+    reader.readAsDataURL(this.camera.files[0]);
+}
+
+// Set the photo taken with the user's camera
+function setPhoto(event){
+    document.getElementById("image").src = event.target.result;
 }
