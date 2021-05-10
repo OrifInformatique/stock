7eee2092d8e0faf3a4ab3409dbc792dc08fe2c04
Cat Sama <david.aeschlimann96@gmail.com>
2021-05-10 08:44:33 +0200

    Emptied the branch

--- database_structure/20180206_DATABASE_STRUCTURE_Update08to10.sql
+++
@@ -1,3 +0,0 @@
--- Update from patch 0.8 to patch 0.10
-
-ALTER TABLE `loan` CHANGE `item_localisation` `item_localisation` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
