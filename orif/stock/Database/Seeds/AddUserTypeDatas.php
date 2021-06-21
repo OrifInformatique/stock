8e885eb3f7735b58a575611705c16a6e84db9bdf
SchindlerSimao <60343456+SchindlerSimao@users.noreply.github.com>
2021-06-21 08:27:13 +0200

    corrected user types names

--- orif/stock/Database/Seeds/AddUserTypeDatas.php
+++ orif/stock/Database/Seeds/AddUserTypeDatas.php
@@ -9,9 +9,11 @@ class AddUserTypeDatas extends \CodeIgniter\Database\Seeder
     public function run()
     {
         $data=[
-            ['name'=>'Administrateur','access_level'=>4],
-            ['name'=>'Enregistré','access_level'=>2],
-            ['name'=>'Invité','access_level'=>1]
+            ['name'=>'Invite','access_level'=>1],
+            ['name'=>'Observation','access_level'=>2],
+            ['name'=>'Formation','access_level'=>4],
+            ['name'=>'MSP','access_level'=>8],
+            ['name'=>'Administrateur','access_level'=>16]            
         ];
         foreach($data as $row)
             $this->db->table('user_type')->insert($row);
