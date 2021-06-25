d0a73e5fe870a508c69ceb45b8452dad6413a634
catdesu <david.aeschlimann96@gmail.com>
2021-06-25 11:17:20 +0200

    Added an ID to user_details table and made sure it has auto_increment attribute

--- orif/stock/Database/Migrations/2021-05-28-131603_add_user_details.php
+++ orif/stock/Database/Migrations/2021-05-28-131603_add_user_details.php
@@ -9,10 +9,18 @@ class Adduserdetails extends Migration
 	public function up()
 	{
 		// Query to clone user table as user_details with wanted rows
-		$this->db->query('CREATE TABLE user_details SELECT user_id AS fk_user, lastname, firstname FROM user');
+		$this->db->query('CREATE TABLE user_details SELECT user_id AS id, user_id AS fk_user, lastname, firstname FROM user');
 
 		// PRIMARY constraint won't be cloned, so we add it manually
-		$this->forge->addColumn('user_details', 'CONSTRAINT user_detail PRIMARY KEY (fk_user)');
+		$this->forge->addColumn('user_details', 'CONSTRAINT user_details PRIMARY KEY (id)');
+
+		// Makes sure id has AUTO INCREMENT and DEFAULT value as none
+		$this->forge->modifyColumn('user_details', [
+			'id'		=> [
+				'type'				=> 'INT',
+				'auto_increment'	=> true
+			]
+		]);
 	}
 
 
