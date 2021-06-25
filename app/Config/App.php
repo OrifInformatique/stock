6cac25d3074d36cbcd373e054583c957c5bd1df5
catdesu <david.aeschlimann96@gmail.com>
2021-06-25 08:19:58 +0200

    Modified App and Database file in Config to avoid conflict

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/stock/public/';
+	public $baseURL = 'http://stock.local/';
 
 	/**
 	 * --------------------------------------------------------------------------
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => 'root',
+		'username' => '',
 		'password' => '',
-		'database' => 'stock',
+		'database' => '',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
