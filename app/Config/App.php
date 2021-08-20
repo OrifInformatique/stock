80d3de6b4fcafd76cde3064ca0a8593cf81803ed
David Aeschlimann <69304783+catdesu@users.noreply.github.com>
2021-08-20 16:11:07 +0200

    Update the database to match the packbase user module

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/ci_packbase_v4/public/';
+	public $baseURL = 'http://localhost/stock/public/';
 
 	/**
 	 * --------------------------------------------------------------------------
---
+++ orif/stock/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Routes for Stock Module
+ *
+ * @author      Orif (ViDi, AeDa)
+ * @link        https://github.com/OrifInformatique
+ * @copyright   Copyright (c), Orif (https://www.orif.ch)
+ */
+
+?>
