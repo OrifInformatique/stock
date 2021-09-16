184e082d654359403376ba44a67c662f3704a144
catdesu <david.aeschlimann96@gmail.com>
2021-09-16 08:14:12 +0200

    Added restore_CI3 back, after unwanted deletion

---
+++ orif/stock/Database/Migrations/restore_CI3_version/2021-06-01-093859_add_ci_sessions.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace  Stock\Database\Migrations;
+
+
+class Add_ci_sessions extends \CodeIgniter\Database\Migration
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function up()
+    {
+        $this->forge->addField([
+            'id'=>[
+                'type'              => 'VARCHAR',
+                'constraint'        => '128',
+                'null'              => false,
+                'auto_increment'    => true
+            ],
+            'ip_address'=>[
+                'type'              => 'VARCHAR',
+                'constraint'        => '45',
+                'null'              => false
+            ],
+            'timestamp'=>[
+                'type'              => 'INT',
+                'constraint'        => '10',
+                'null'              => false,
+                'default'           => 0,
+                'unsigned'          => true
+            ],
+            'data'=>[
+                'type'              => 'blob',
+                'null'              => false
+            ]
+        ]);
+
+        $this->forge->addKey('id', TRUE);
+        
+        
+        $this->forge->createTable('ci_sessions', TRUE);
+ 
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down()
+    {
+        $this->forge->dropTable('ci_sessions');
+    }
+}
