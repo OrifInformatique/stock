c13570cf1915c9221be003d4dc4bd9ae9f3b2411
Didier Viret <didier.viret@orif.ch>
2021-11-25 17:09:29 +0100

    Delete migration files to use only once in v4.0

--- orif/stock/views/admin/clean_images/success.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-    <div class="row" >
-      <?= lang('MY_application.admin_command_clean_images_success'); ?>
-    </div>
-    <div class="btn-group row" >
-      <a href="<?= base_url(); ?>" class="btn btn-lg"><?= lang('MY_application.btn_back_to_main'); ?></a>
-    </div>
-</div>
