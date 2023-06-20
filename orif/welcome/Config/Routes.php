e5d4c9b096d3f0b16f8bb4cc46005a144ebadd11
catdesu <david.aeschlimann96@gmail.com>
2023-06-20 08:15:14 +0200

    Remove Welcome module

--- orif/welcome/Config/Routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$routes->group('welcome',function($routes){
-    $routes->add('home','\Welcome\Controllers\Home');
-    $routes->add('home/(:any)','\Welcome\Controllers\Home::$1');
-});
-
-?>
