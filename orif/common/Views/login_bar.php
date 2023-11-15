561f0ba85b82eaaf0b5296d5c3612b4eaa07e12d
DidierPerroud <didier.perroud@formation.orif.ch>
2023-11-15 11:16:24 +0100

    Saving current URL for redirection

--- orif/common/Views/login_bar.php
+++ orif/common/Views/login_bar.php
@@ -38,7 +38,7 @@
 
         <?php } else { ?>
           <!-- Not logged in, display a "login" button -->
-          <a href="<?php echo base_url("user/auth/login"); ?>" ><?php echo lang('common_lang.btn_login'); ?></a>
+          <a href="<?php echo base_url("user/auth/login?after_login_redirect=" . current_url()); ?>"><?php echo lang('common_lang.btn_login'); ?></a>
         <?php } ?>
       </div>
     </div>
