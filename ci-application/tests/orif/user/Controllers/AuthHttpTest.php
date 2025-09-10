905d0e0d2f89cbb912735fd6d0a1261f877965a4
Didier Viret <didier.viret@orif.ch>
2025-09-10 15:30:00 +0200

    fix: remove output buffering for AuthHttpTest.php

--- ci-application/tests/orif/user/Controllers/AuthHttpTest.php
+++ ci-application/tests/orif/user/Controllers/AuthHttpTest.php
@@ -38,8 +38,6 @@ class AuthHttpTest extends CIUnitTestCase
 
     public function test_azure_mail_with_correct_code_new_user(): void
     {
-        ob_start(); // Start output buffering
-
         $firstName = 'Firstname';
         $lastName = 'Lastname';
         $userName = "$firstName.$lastName";
@@ -56,14 +54,10 @@ class AuthHttpTest extends CIUnitTestCase
         $name = $userModel->select('username')->where('username=', $userName)
                                               ->findAll()[0]['username'];
         $this->assertEquals($userName, $name);
-
-        ob_end_clean(); // Clear the output buffer
     }
 
     public function test_azure_mail_existed_user_variable_created(): void
     {
-        ob_start(); // Start output buffering
-
         $userId = 2;
         $noAzureMail = 'fake@fake.fake';
         $userModel = model(User_model::class);
@@ -80,14 +74,10 @@ class AuthHttpTest extends CIUnitTestCase
         $result = $this->withSession()->post($url);
         # d($result->response()->getBody());
         $result->assertSee(lang('user_lang.user_validation_code'));
-
-        ob_end_clean(); // Clear the output buffer
     }
 
     public function test_azure_mail_with_correct_code_existing_user(): void
     {
-        ob_start(); // Start output buffering
-
         $userId = 2;
         $noAzureMail = 'fake@fake.fake';
         $userModel = model(User_model::class);
@@ -107,7 +97,5 @@ class AuthHttpTest extends CIUnitTestCase
         $azureMailInDb = $userModel->select('azure_mail')
                                ->find($userId)['azure_mail'];
         $this->assertEquals($azureMail, $azureMailInDb);
-
-        ob_end_clean(); // Clear the output buffer
     }
 }
